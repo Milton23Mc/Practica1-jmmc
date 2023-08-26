34e061a5eb5a0dba866ec3b8f1c92aa461c36e6e
Milton23Mc <josemilton1274@gmail.com>
2023-08-25 21:45:39 -0400

    Soluciones

--- sigleresponsability.ts
+++ sigleresponsability.ts
@@ -1,4 +1,29 @@
-class Libro1 {
+// EJERCICIO DADO
+
+// class Libro1 {
+//     titulo: string;
+//     autor: string;
+//     contenido: string;
+
+//     constructor(titulo: string, autor: string, contenido: string) {
+//         this.titulo = titulo;
+//         this.autor = autor;
+//         this.contenido = contenido;
+//     }
+//     mostrarContenido () {
+//         return this.contenido;
+//     }
+//     guardarEnBaseDeDatos () {
+//     // Código para guardar el libro en la base de datos
+//         console.log(`Guardando el libro ${this.titulo} en la base de datos...`);
+//     }
+// }
+// const libro1 = new Libro1('El principito', 'Antoine de Saint-Exupéry', 'Érase una vez...');
+// libro1.guardarEnBaseDeDatos();
+
+
+
+class Libro {
     titulo: string;
     autor: string;
     contenido: string;
@@ -8,13 +33,19 @@ class Libro1 {
         this.autor = autor;
         this.contenido = contenido;
     }
-    mostrarContenido () {
+    
+    mostrarContenido() {
         return this.contenido;
     }
-    guardarEnBaseDeDatos () {
-    // Código para guardar el libro en la base de datos
-        console.log(`Guardando el libro ${this.titulo} en la base de datos...`);
+}
+
+class BaseDeDatos {
+    guardarLibro(libro: Libro) {
+        // Código para guardar el libro en la base de datos
+        console.log(`Guardando el libro ${libro.titulo} en la base de datos...`);
     }
 }
-const libro1 = new Libro1('El principito', 'Antoine de Saint-Exupéry', 'Érase una vez...');
-libro1.guardarEnBaseDeDatos();
+
+const libro1 = new Libro('El principito', 'Antoine de Saint-Exupéry', 'Érase una vez...');
+const baseDeDatos = new BaseDeDatos();
+baseDeDatos.guardarLibro(libro1);
