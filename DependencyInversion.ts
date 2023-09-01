3c230f2e294f8f3a08bf7e6526103a4b6bebb23f
Milton23Mc <josemilton1274@gmail.com>
2023-09-01 16:56:36 -0400

    Update DependencyInversion.ts

--- DependencyInversion.ts
+++ DependencyInversion.ts
@@ -20,3 +20,34 @@ class Interruptor {
         this.luz.encender(); // o this.luz.apagar();
     }
 }
+
+
+
+
+class BaseDeDatos {
+    guardar(configuracion: Configuracion) {
+        console.log(`Guardando configuración con valor: ${configuracion.valor} en la base de datos`);
+    }
+    obtener(): Configuracion {
+        // Obtaner de la Base de Datos.....
+        return new Configuracion("valor");
+    }
+}
+
+
+class GestorConfiguraciones {
+    db: BaseDeDatos;
+
+    constructor() {
+        this.db = new BaseDeDatos();
+    }
+
+    guardarConfiguracion(valor: string) {
+        const configuracion = new Configuracion(valor);
+        this.db.guardar(configuracion);
+    }
+
+    obtenerConfiguracion(): Configuracion {
+        return this.db.obtener();
+    }
+}
